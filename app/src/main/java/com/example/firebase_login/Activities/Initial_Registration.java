14280d3cc81eaa71c95f1ef60723c3bd292870a1
harshitpanwar <54681032+harshitpanwar@users.noreply.github.com>
2021-05-03 22:27:00 +0530

    Added new Actitvity -  Initial_Registration.java  activity_initial__registration.xml

---
+++ app/src/main/java/com/example/firebase_login/Activities/Initial_Registration.java
@@ -0,0 +1,16 @@
+package com.example.firebase_login.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.firebase_login.R;
+
+public class Initial_Registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial__registration);
+    }
+}
